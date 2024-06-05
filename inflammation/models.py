1a12f0f832c980bc625094843da501722cb4e88a
Magnus Hagdorn <magnus.hagdorn@charite.de>
2024-06-05 14:00:46 +0200

    chore: make pylint happy

--- inflammation/models.py
+++ inflammation/models.py
@@ -49,9 +49,9 @@ def patient_normalise(data):
     """Normalise patient data from a 2D inflammation data array."""
     if np.any(data < 0):
         raise ValueError("Inflammation values should be negative")
-    max = np.nanmax(data, axis=1)
+    max_inflammation = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / max_inflammation[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
